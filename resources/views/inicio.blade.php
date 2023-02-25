095da64a6866ebee9364e0b71b68f65005451726
Fillipe Anjos <fillipe.anjos.2000@hotmail.com>
2023-02-25 10:20:28 -0300

    Inclusão da pagina inicio

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -1,3 +1,36 @@
-<h1>Pagina Inicial</h1>
+@extends('layouts.main')
 
-<a href="/">Voltar para a Home</a>
+@section('title', 'Página Inicial')
+
+    <style>
+        
+    </style>
+
+@section('content')
+
+
+    <br/>
+
+    
+
+
+    <div class="container">
+        <div class="row">
+            <div class="col">
+
+                <span>Faça seu login aqui, no link abaixo</span>
+                <br/>
+
+                <a href="/login">Login</a>
+            </div>
+            <div class="col">
+
+                <span>Faça seu registro aqui, no link abaixo</span>
+                <br/>
+
+                <a href="/register">Registrar</a>
+            </div>
+        </div>
+    </div>
+
+@endsection
